f4bb8c81d8a175905c1d3ba0defb2adef14895e8
BloodMoon <relaxx022@gmail.com>
2023-04-03 13:44:49 +0530

    Initial commit

---
+++ src/main/java/net/lunatic/anemomod/AnemoMod.java
@@ -0,0 +1,16 @@
+package net.lunatic.anemomod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AnemoMod implements ModInitializer {
+
+	public static final String MOD_ID = "anemomod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/lunatic/anemomod/AnemoModClient.java
@@ -0,0 +1,10 @@
+package net.lunatic.anemomod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AnemoModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}

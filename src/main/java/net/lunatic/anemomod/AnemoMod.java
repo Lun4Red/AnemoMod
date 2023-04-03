5c8d330515b39d9d7ddb7b6c057b4a512b101188
BloodMoon <relaxx022@gmail.com>
2023-04-03 13:51:28 +0530

    testing commits

--- src/main/java/net/lunatic/anemomod/AnemoMod.java
+++ src/main/java/net/lunatic/anemomod/AnemoMod.java
@@ -5,7 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class AnemoMod implements ModInitializer {
-
+	//test comment
 	public static final String MOD_ID = "anemomod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
